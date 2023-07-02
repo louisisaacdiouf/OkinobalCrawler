e5cdf8a0ec758f7d5d5d5a0243a11f13a41ea689
Louis Isaac DIOUF <61230778+louisisaacdiouf@users.noreply.github.com>
2023-07-02 18:40:57 +0000

    Add files via upload

--- okinobalCrawler.py
+++ okinobalCrawler.py
@@ -3,48 +3,55 @@
 import argparse
 import re
 import requests
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
+
+def is_internal_link(url, base_url):
+    parsed_url = urlparse(url)
+    parsed_base_url = urlparse(base_url)
+    return parsed_url.netloc == parsed_base_url.netloc
+
+def is_valid_link(url, base_url):
+    file_extensions = (".jpg", ".jpeg", ".png", ".gif", ".pdf")
+    invalid_keywords = (".css", "javascript", "mailto:", "tel:", "/ads", "file", "upload", "photo", "image", "video")
+    parsed_url = urlparse(url)
+
+    if parsed_url.scheme and parsed_url.netloc:
+        return is_internal_link(url, base_url) and not url.endswith(file_extensions)
+    else:
+        return not any(keyword in url for keyword in invalid_keywords) and (url.endswith("/") or url.endswith(".html") or urljoin(base_url, url).endswith("/"))
 
 def scan_links(html_content, base_url):
     soup = BeautifulSoup(html_content, "html.parser")
     links = []
     for link in soup.find_all("a"):
         href = link.get("href")
-        if href is not None:
-            if not any(substring in href for substring in [".jpg", ".jpeg", ".png", ".gif", ".pdf", ".css","javascript", "mailto:", "tel:", "/ads", "file", "upload", "photo", "image", "video"]) and (href.endswith(".html") or href.endswith(".html/") or (not any(char in href for char in ["?", "=", "#"]) and len([c for c in href if c == "/"]) <= 1 and not href.startswith("/") and not href.startswith("http"))):
-                if href.startswith("/"):
-                    href = base_url + href
-                elif not href.startswith(base_url):
-                    href = base_url + "/" + href
-                links.append(href)
+        if href is not None and is_valid_link(href, base_url):
+            absolute_url = urljoin(base_url, href)
+            links.append(absolute_url)
     return links
 
 def scan_website(base_url):
-    if base_url[-1] == "/":
-        start_url = base_url
-    else:
-        start_url = base_url + "/"
-
-    visited = []
+    visited = set()
     links = []
-    queue = [start_url]
+    queue = [base_url]
+
     while queue:
         url = queue.pop(0)
-        visited.append(url)
+        visited.add(url)
         print(url)
+
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 html_content = response.text
-                new_links = scan_links(html_content, base_url)
+                new_links = scan_links(html_content, url)
                 for link in new_links:
                     if link not in links and link not in visited:
                         links.append(link)
-                        print(link)
                         queue.append(link)
         except:
             pass
-    return links
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Website Crawler")
